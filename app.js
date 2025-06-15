8e02fc5ce49469130e7fcb61323626e59c250e5c
Andrew Paton <arp3000@hw.ac.uk>
2025-06-15 22:52:20 +0100

    Added basic html login and chore to do section, and basic css

--- app.js
+++ app.js
@@ -9,3 +9,5 @@ const supabase = supabase.createClient(
   'https://qwdwcsdgjhuthhrpijoz.supabase.co',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3ZHdjc2Rnamh1dGhocnBpam96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMjIzNTEsImV4cCI6MjA2NTU5ODM1MX0.oh2bBUPY_YJQkFWPy06JSA_B-sXkg1h5t0DoK1eqnrQ'
 );
+
+
