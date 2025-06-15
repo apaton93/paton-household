97d9573719cd50146ea06dd594fda426c027e4a3
Andrew Paton <arp3000@hw.ac.uk>
2025-06-15 22:31:09 +0100

    Added initial files and supabase js code

---
+++ app.js
@@ -0,0 +1,9 @@
+//https://qwdwcsdgjhuthhrpijoz.supabase.co
+//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3ZHdjc2Rnamh1dGhocnBpam96Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMjIzNTEsImV4cCI6MjA2NTU5ODM1MX0.oh2bBUPY_YJQkFWPy06JSA_B-sXkg1h5t0DoK1eqnrQ
+
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://qwdwcsdgjhuthhrpijoz.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
+const supabase = createClient(supabaseUrl, supabaseKey)
